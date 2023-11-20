566dc913627d3f61a4cda52b64701a4b7e6fa02a
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 19:20:51 -0300

    Navbar v4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,30 @@ function Navbar() {
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
-                    <div class="collapse navbar-collapse position-relative px-5 py-3 fs-5" id="navbarNavAltMarkup">
+                    <img src="./images/nombre1.png" alt="Nombre" class="d-lg-none float-center"/>
+                    <div class="collapse navbar-collapse px-5 py-3 fs-5" id="navbarNavAltMarkup">
                         <div class="navbar-nav container">
                             <NavLink to="/projects" activeClassName="active" className="nav-link" aria-current="page">Projects</NavLink>
                             <NavLink to="/about" activeClassName="active" className="nav-link" aria-current="page">About me</NavLink>
                             <NavLink to="/contact" activeClassName="active" className="nav-link" aria-current="page">Contact</NavLink>
 
-                            <div class="h5 navbar-collapse">
+                            <div class="h5 d-lg-none" id="redes">
+                                <ul class="navbar-collapse nav justify-content-center">
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="https://www.linkedin.com/in/diaconchukm/" target='blank'><i class="fa-brands fa-linkedin"></i></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="https://www.instagram.com/matidiaconchuk/" target='blank'><i class="fa-brands fa-instagram"></i></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/contact"><i class="fa-solid fa-envelope"></i></a>
+                                    </li>
+                                </ul>
+                                <div></div>
+                            </div>
+
+                                
+                            <div class="h5 collapse navbar-collapse" id="redes2">
                                 <ul class="navbar-collapse nav justify-content-end">
                                     <li class="nav-item">
                                         <a class="nav-link" aria-current="page" href="https://www.linkedin.com/in/diaconchukm/" target='blank'><i class="fa-brands fa-linkedin"></i></a>
@@ -27,10 +43,11 @@ function Navbar() {
                                         <a class="nav-link" href="https://www.instagram.com/matidiaconchuk/" target='blank'><i class="fa-brands fa-instagram"></i></a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="/contact" target='blank'><i class="fa-solid fa-envelope"></i></a>
+                                        <a class="nav-link" href="/contact"><i class="fa-solid fa-envelope"></i></a>
                                     </li>
                                 </ul>
                             </div>
+
                         </div>
                         <div>
                         </div>
