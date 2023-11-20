a47e05f83c3cf4fb8a3ef808c60f7ec2529ef7e8
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 17:04:01 -0300

    Navbar v3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import About from './About'
 import Projects from './Projects'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-body-secondary">
-                <div class="container-fluid h-25">
+                <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse position-relative px-5 py-3 fs-5" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link" aria-current="page" href="/projects">Projects</a>
-                            <a class="nav-link" href="/about">About</a>
-                            <a class="nav-link" href="/contact">Contact</a>
+                        <div class="navbar-nav container">
+                            <NavLink to="/projects" activeClassName="active" className="nav-link" aria-current="page">Projects</NavLink>
+                            <NavLink to="/about" activeClassName="active" className="nav-link" aria-current="page">About me</NavLink>
+                            <NavLink to="/contact" activeClassName="active" className="nav-link" aria-current="page">Contact</NavLink>
+
+                            <div class="h5 navbar-collapse">
+                                <ul class="navbar-collapse nav justify-content-end">
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="https://www.linkedin.com/in/diaconchukm/" target='blank'><i class="fa-brands fa-linkedin"></i></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="https://www.instagram.com/matidiaconchuk/" target='blank'><i class="fa-brands fa-instagram"></i></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/contact" target='blank'><i class="fa-solid fa-envelope"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                         <div>
                         </div>
-                    </div>
-
-                    <div class="h5 navbar-collapse justify-content-end px-5">
-                        <ul class="navbar-collapse nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="https://www.linkedin.com/in/diaconchukm/"><i class="fa-brands fa-linkedin"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="https://www.instagram.com/matidiaconchuk/"><i class="fa-brands fa-instagram"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/contact"><i class="fa-solid fa-envelope"></i></a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </nav>
