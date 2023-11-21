c9b08eec2769b4ac87fc27e069fcc20c065c1df0
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 23:14:57 -0300

    Contact casi hecho, fin dia 1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <Navbar />
       <div class="container border border-1 rounded py-5 my-3 bgtext bgwave shadow-lg h-75">
         <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> <span class="bg-body-secondary rounded mx-1">You can contact me freely at </span> <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span></p>
-        <img src="../images/Gmail.png" class="my-3 h-25 w-25" />
+        <img src="./images/Gmail.png" class="my-3 h-25 w-25" />
 
         {/*  INTRODUCCIÓN ARRIBA */}
 
@@ -20,7 +20,7 @@ function Contact() {
         <div class="mb-3">
           <textarea class="form-control w-75 m-auto bg-body-tertiary" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
         </div>
-        <button type="button" class="btn btn-success px-5 rounded-pill">Submit</button> 1
+        <button type="button" class="btn btn-success px-5 rounded-pill">Submit</button>
       </div>
     </div>
   )
