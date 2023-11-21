32ecff1f11cbb061468f4b51c599267fe9e95acf
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 23:21:08 -0300

    Contact casi hecho

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <Navbar />
       <div class="container border border-1 rounded py-5 my-3 bgtext bgwave shadow-lg h-75">
         <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> <span class="bg-body-secondary rounded mx-1">You can contact me freely at </span> <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span></p>
-        <img src="./images/Gmail.png" class="my-3 h-25 w-25" />
+        <img src="../images/Gmail.png" />
 
         {/*  INTRODUCCIÓN ARRIBA */}
 
