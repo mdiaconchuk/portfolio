5b54ab223ee2b2e1e5f327d6bcf65e6536137eea
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-21 18:34:59 -0300

    Contact estético hecho

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,15 +12,18 @@ function Contact() {
         {/*  INTRODUCCIÓN ARRIBA */}
 
         <div class="mb-3">
-          <input type="email" class="bg-body-tertiary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your name..."/>
+          <input type="email" class="bg-body-secondary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your name..." />
         </div>
         <div class="mb-3">
-          <input type="email" class="bg-body-tertiary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your mail..."/>
+          <input type="email" class="bg-body-secondary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your mail..." />
         </div>
         <div class="mb-3">
-          <textarea class="form-control w-75 m-auto bg-body-tertiary" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
+          <textarea class="form-control w-75 m-auto bg-body-secondary" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
         </div>
+        {/* FORMULARIO */}
+
         <button type="button" class="btn btn-success px-5 rounded-pill">Submit</button>
+        {/*  SUBMIT BUTTON */}
       </div>
     </div>
   )
