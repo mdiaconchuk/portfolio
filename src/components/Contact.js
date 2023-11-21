e5176c79efb3973c5f38f2969af6ad865bdb3792
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 23:03:40 -0300

    Contact casi hecho

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,21 +5,22 @@ function Contact() {
   return (
     <div>
       <Navbar />
-      <div class="container border border-2 rounded border-success py-5 my-5">
-        <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> You can contact me freely at <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span>.</p>
-        <img src="./images/Gmail.png" class="my-3 h-25 w-25" />
+      <div class="container border border-1 rounded py-5 my-3 bgtext bgwave shadow-lg h-75">
+        <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> <span class="fw-bold bg-body-secondary rounded mx-1">You can contact me freely at </span> <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span></p>
+        <img src="../images/Gmail.png" class="my-3 h-25 w-25" />
 
         {/*  INTRODUCCIÓN ARRIBA */}
 
         <div class="mb-3">
-          <input type="email" class="bg-secondary-subtle form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your name..."/>
+          <input type="email" class="bg-body-tertiary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your name..."/>
         </div>
         <div class="mb-3">
-          <input type="email" class="bg-secondary-subtle form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your mail..."/>
+          <input type="email" class="bg-body-tertiary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your mail..."/>
         </div>
         <div class="mb-3">
-          <textarea class="form-control w-75 m-auto bg-secondary-subtle" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
-        </div> 1
+          <textarea class="form-control w-75 m-auto bg-body-tertiary" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
+        </div>
+        <button type="button" class="btn btn-success px-5 rounded-pill">Submit</button>
       </div>
     </div>
   )
