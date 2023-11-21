d9550def3a6e44c7c1c1ebdb35bbc449fa753498
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-20 22:01:46 -0300

    Contact sencillo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,11 @@ import Navbar from './Navbar'
 function Contact() {
   return (
     <div>
-    <Navbar/>
-    Contact
+      <Navbar />
+      <div class="container border border-2 rounded border-success py-5 my-5">
+        <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> You can contact me freely at <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span>.</p>
+        <img src="./images/Gmail.png" class="my-3 h-25 w-25" />
+      </div>
     </div>
   )
 }
