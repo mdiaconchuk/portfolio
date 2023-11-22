6d8c46e8ceca7da230ae2927efa570e53e057477
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-21 22:50:04 -0300

    Contact semi-terminado

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Navbar from './Navbar'
+import emailjs from 'emailjs-com'
+import { useState } from 'react'
 
 function Contact() {
+  const [buttonText, setButtonText] = useState('Submit');
+
+  function enviarMail(e) {
+    const form = document.getElementById('formContact')
+    let nombre = document.getElementsByName('user_name')
+    let mail = document.getElementsByName('user_mail')
+    let correo = document.getElementsByName('message')
+    let submit = document.getElementById('submitButton')
+
+    try {
+      e.preventDefault();
+      emailjs.sendForm('service_2wsxgmb', 'template_q4hwmh9', form, 'oZL4atEcfGgQey3n5');
+      setButtonText('Done');
+      submit.className= "btn btn-success px-5 rounded-pill";
+    }
+    catch(err) {
+      alert(err)
+   }
+  }
+
   return (
     <div>
       <Navbar />
@@ -11,19 +33,22 @@ function Contact() {
 
         {/*  INTRODUCCIÓN ARRIBA */}
 
-        <div class="mb-3">
-          <input type="email" class="bg-body-secondary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your name..." />
-        </div>
-        <div class="mb-3">
-          <input type="email" class="bg-body-secondary form-control w-75 m-auto" id="exampleFormControlInput1" placeholder="Your mail..." />
-        </div>
-        <div class="mb-3">
-          <textarea class="form-control w-75 m-auto bg-body-secondary" id="exampleFormControlTextarea1" rows="3" placeholder='Your message...'></textarea>
-        </div>
-        {/* FORMULARIO */}
+        <form id="formContact" onSubmit={enviarMail}>
+          <div class="mb-3">
+            <input type="text" class="bg-body-secondary form-control w-75 m-auto" name="user_name" placeholder="Your name..." required/>
+          </div>
+          <div class="mb-3">
+            <input type="email" class="bg-body-secondary form-control w-75 m-auto" name="user_mail" placeholder="Your mail..." required/>
+          </div>
+          <div class="mb-3">
+            <textarea class="form-control w-75 m-auto bg-body-secondary" name="message" rows="3" placeholder='Your message...' required></textarea>
+          </div>
+          {/* FORMULARIO */}
 
-        <button type="button" class="btn btn-success px-5 rounded-pill">Submit</button>
-        {/*  SUBMIT BUTTON */}
+          <button type="submit" value="Send" id="submitButton" className={'btn btn-info px-5 rounded-pill'}>{buttonText}</button>
+
+          {/*  SUBMIT BUTTON */}
+        </form>
       </div>
     </div>
   )
