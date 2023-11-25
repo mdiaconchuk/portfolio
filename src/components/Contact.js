cfa9b4f08855ef0aa3abb0c7e2226bec9807fa80
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-25 18:39:33 -0300

    Fix menor bg contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,8 +28,8 @@ function Contact() {
   return (
     <div>
       <Navbar />
-      <div class="w-100 border border-1 rounded py-5 bgtext bg-light shadow-lg h-50 bg-light-subtle bgwave">
-        <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> <span class="rounded mx-1 bg-body-secondary">You can contact me freely at </span> <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span></p>
+      <div class="w-100 py-5 bgtext shadow-lg h-50 my-5 bggris">
+        <p class="fs-4"><i class="text-danger fa-regular fa-envelope"></i> <span class="rounded mx-1 text-light">You can contact me freely at </span> <span class="bg-danger-subtle rounded text-danger">mdiaconchuk@gmail.com</span></p>
         <img class="w-25 my-2" src="./assets/images/Gmail.png" />
 
         {/*  INTRODUCCIÓN ARRIBA */}
