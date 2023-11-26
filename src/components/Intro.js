2dc1a7c3f7e20b65feab10e757fd21bf66b77fd8
MDiacon205 <matiasdiaconchuk205@gmail.com>
2023-11-26 00:54:14 -0300

    Fix menor

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,11 +3,12 @@ import React from 'react'
 function Intro() {
     return (
         <div id="intro">
+
             <div class="text-white py-5 w-100 text-dark bg-opacity-100 fw-bold">
                 <p class="fs-1 my-5 mx-5">Hola, soy</p>
                 <p class="fs-1 text-decoration-underline mx-5">Matías Diaconchuk.</p><br /><br /><br /><br /><br />
 
-                <p class="fs-4 my-5">Un desarrollador Front-End de <span class="text-info">Argentina</span>,<br /> que disfruta crear aplicaciones web simples y fáciles de usar.</p>
+                <p class="fs-4 my-5 mx-4">Un desarrollador Front-End de <span class="text-info">Argentina</span>,<br /> que disfruta crear aplicaciones web simples y fáciles de usar.</p>
                 <br /><br /><br /><br /><br /><br /><br />
                 {/* <div class="container text-center"> <br /><br /><br />
                     <div class="row p-auto py-5">
